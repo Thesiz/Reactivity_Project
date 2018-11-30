7bd98d4c6d72645633510c9bced16a87182ec446
Thesiz <32890516+CrewDeNegro@users.noreply.github.com>
2018-11-30 14:22:22 -0500

    Creando la interfaz e inicio
    Canvas, plantilla raw image, main menu
    some scripts...

--- Reactivity/Assets/Scripts/GameController.cs
+++ Reactivity/Assets/Scripts/GameController.cs
@@ -2,33 +2,67 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour {
 
+	public static GameController estadoJuego;
+
 	//TODO: agrear efecto parallax en funcion de movimiento de jugador y no en cada frame
+	//parallax
 	public float parallaxSpeed = 0.2f;
 	public RawImage background;
 	public RawImage texturas;
 
-	public enum GameState {stop, playing}
-	public GameState gameState = GameState.stop;	
-	// Use this for initialization
+	//gamestate
+
+	public enum GameState {Stop, Playing}
+	public GameState gameState = GameState.Stop;
+
+	void Awake(){
+		if (estadoJuego == null) {
+			estadoJuego = this;
+			DontDestroyOnLoad (gameObject);
+		} else if (estadoJuego != this) {
+			Destroy (gameObject);
+		}
+
+	}
+
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
 
 		//Comienza el juego
-		if(gameState == GameState.stop && (Input.GetKeyDown("up") || Input.GetKeyDown("x") || Input.GetMouseButtonDown(0))){
-			gameState = GameState.playing;
+		if (gameState == GameState.Stop && (Input.GetKeyDown ("x") || Input.GetMouseButtonDown (0))) {
+			gameState = GameState.Playing;
+
+
+		} //juego en marcha
+		else if (gameState == GameState.Playing) {
+			//TODO: cambiar a nivel_1
+			CambiarEscena("level_1");
+
+		} else {
+			Parallax ();
 		}
+	
+	}
 
 
+	public void Parallax () {
 		float finalSpeed = parallaxSpeed * Time.deltaTime;
 		background.uvRect = new Rect (background.uvRect.x + finalSpeed, 0f, 1f, 1f);
 		texturas.uvRect = new Rect (texturas.uvRect.x + finalSpeed * 4, 0f, 1f, 1f);
-		
 	}
+
+
+	public void CambiarEscena(string sceneName){
+		print ("Cambiando a " + sceneName);
+		SceneManager.LoadScene (sceneName);
+
+	}
+		
 }
